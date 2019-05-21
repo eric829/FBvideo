5534e66cba5c9d46ce7585aa1a7a6afdc6b72ff3
eric829 <eric78829@gmail.com>
2019-05-21 22:37:41 +0800

    make it better

---
+++ FBdownload.py
@@ -0,0 +1,30 @@
+import requests
+import re
+from flask import Flask, request
+import shutil
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST']) 
+def download():
+    if request.method == 'POST': 
+        res = requests.get(request.values['url'])
+        s = re.search('hd_src:"(https://.*?)",',res.text)
+        res2 = requests.get(s.group(1),stream =True)
+        f = open(request.values['filename']+'.mp4','wb')
+        shutil.copyfileobj(res2.raw,f)
+        f.close
+        return 'Download is finish'
+
+    return "</br>" \
+            "</br>" \
+            "</br>" \
+            "</br>" \
+            "</font><form align='center' method='post' action='/'><font size='7' color='blue'>Url     <input type='text' name='url' />" \
+            "</br>"\
+            "<font size='7' color='blue'>Filename</font> <form align='center' method='post' action='/'> <input type='text' name='filename' />" \
+            "</br>" \
+            "<button type='submit'>Submit</button></form>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FBdownload:app --log-file -
